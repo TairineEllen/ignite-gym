7b6bce16f4a3c504ab2fe23ea69b0e4f56f4f8d1
TairineEllen <tairine.ellen@gmail.com>
2025-05-07 16:04:19 -0300

    add scrollview

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed";
+import { VStack, Image, Center, Text, Heading, ScrollView } from "@gluestack-ui/themed";
 import BackgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
 import { Input } from "@components/Input";
@@ -6,7 +6,8 @@ import { Button } from "@components/Button";
 
 export function SignIn() {
     return (
-        <VStack flex={1} bg="$gray700">
+        <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
+               <VStack flex={1} bg="$gray700">
             <Image
                 w="$full"
                 h={ 624}
@@ -45,5 +46,7 @@ export function SignIn() {
             
 
         </VStack>
+        </ScrollView>
+     
     )
 }
