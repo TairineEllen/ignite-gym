3febbe81c46a4e68e50c70555fee14c842053283
TairineEllen <tairine.ellen@gmail.com>
2025-05-07 16:09:55 -0300

    add screen of sign up

--- App.tsx
+++ App.tsx
@@ -4,13 +4,14 @@ import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular})
   return (
    <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <SignIn />: <Loading />}
+      {fontsLoaded ? <SignUp />: <Loading />}
    </GluestackUIProvider>
   );
 }
