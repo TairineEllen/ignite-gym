4bdf2c6288ca3d98ce76c99831e5063a5e1e7995
TairineEllen <tairine.ellen@gmail.com>
2025-05-07 16:00:52 -0300

    add buttons

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,6 +2,7 @@ import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed";
 import BackgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
     return (
@@ -27,7 +28,15 @@ export function SignIn() {
                 <Heading color="$gray100">Acesse sua conta</Heading>
                 <Input placeholder="Email" keyboardType="email-address" autoCapitalize="none"/>
                 <Input placeholder="Senha" secureTextEntry/>
+                <Button title="Acessar"/>
 
+            </Center>
+
+            <Center flex={1} justifyContent="flex-end" mt="$4">
+                <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$sm">
+                    Ainda não tem acesso?
+                </Text>
+                <Button title="Criar conta" variant="outline"/>
             </Center>
             </VStack>
 
