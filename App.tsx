1dad8e52846ec922b5a6b12a27f1f85ab949b6ae
TairineEllen <tairine.ellen@gmail.com>
2025-05-07 15:23:55 -0300

    add background image and logo

--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/
 import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular})
   return (
    <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <Center flex={1} bg="$info600"><Text>Home</Text></Center> : <Loading />}
+      {fontsLoaded ? <SignIn />: <Loading />}
    </GluestackUIProvider>
   );
 }
