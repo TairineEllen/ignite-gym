72be9394a269eda0177fd7c1592ca84e5af1832d
TairineEllen <tairine.ellen@gmail.com>
2025-05-13 16:15:54 -0300

    add navigation context

--- App.tsx
+++ App.tsx
@@ -3,15 +3,14 @@ import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/
 import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
-import { SignUp } from '@screens/SignUp';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular})
   return (
    <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <SignUp />: <Loading />}
+      {fontsLoaded ? <Routes />: <Loading />}
    </GluestackUIProvider>
   );
 }
