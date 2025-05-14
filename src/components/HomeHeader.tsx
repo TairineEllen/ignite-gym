3539560d6df874d0f37adf7fb99bf876449fbef0
TairineEllen <tairine.ellen@gmail.com>
2025-05-14 18:14:05 -0300

    add component user photo

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,19 @@
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
     return (
-        <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" style={{position: "absolute", top: 0, left: 0, right: 0}}>
+        <HStack
+            bg="$gray600" 
+            pt="$16" pb="$5" px="$8" alignItems="center" 
+            style={{position: "absolute", top: 0, left: 0, right: 0}}
+            gap="$4">
+            <UserPhoto
+                source={{ uri: "https://github.com/TairineEllen.png"}}
+                alt="Imagem do usuário"
+                h="$16"
+                w="$16"
+            />
             <VStack>
                 <Text color="$gray100" fontSize="$sm">
                 Olá,
