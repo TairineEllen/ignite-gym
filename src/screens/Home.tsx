0ea88b8aa8dbf5a4a85bb05558e67b746501d551
TairineEllen <tairine.ellen@gmail.com>
2025-05-15 14:28:04 -0300

    add navigation to exercise

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,8 @@ import { ExerciseCard } from '@components/ExerciseCard';
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 import { useState } from 'react'
 import { FlatList } from 'react-native';
 
@@ -17,7 +19,14 @@ export function Home() {
       "Remada curvadaa", 
       "Remada unilaterall", 
       "Levantamento terraa",
-    ])
+    ]);
+
+    const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+    function handleOpenExerciseDetails() {
+      navigation.navigate("exercise")
+    }
+
   return (
     <VStack flex={1}>
       <HomeHeader />
@@ -47,7 +56,7 @@ export function Home() {
           <FlatList
             data={exercises}
             keyExtractor={item => item}
-            renderItem={() => <ExerciseCard />}
+            renderItem={() => <ExerciseCard onPress={handleOpenExerciseDetails} />}
             showsVerticalScrollIndicator={ false}
             contentContainerStyle={{ paddingBottom: 20}}
           />
