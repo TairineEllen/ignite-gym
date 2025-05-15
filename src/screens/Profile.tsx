8aa419d860c1541ab62e6b0c767b38f48b7998f2
TairineEllen <tairine.ellen@gmail.com>
2025-05-15 19:55:20 -0300

    change user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -3,19 +3,40 @@ import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
 import { ScrollView, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 export function Profile() {
+    const [userPhoto, setUserPhoto] = useState("https:github.com/TairineEllen.png");
+
+    async function handleUserPhotoSelect() {
+       const photoSelected =  await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: [
+                "images"
+            ],
+            quality: 1,
+            aspect: [4, 4],
+            allowsEditing: true
+        })
+
+        if (photoSelected.canceled) {
+            return;
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+    }
+
     return (
         <VStack flex={1}>
             <ScreenHeader title="Perfil" />
             <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
                 <Center mt="$6" px="$10">
                     <UserPhoto
-                        source={{ uri: "https:github.com/TairineEllen.png"}}
+                        source={{ uri: userPhoto }}
                         alt="Foto do usuário"
                         size="xl"
                     />
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleUserPhotoSelect}>
                         <Text color="$green500" fontFamily="$heading" fontSize="$md" mt="$2" mb="$8">
                             Alterar foto
                         </Text>
