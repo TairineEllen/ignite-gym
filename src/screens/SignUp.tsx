b617723ab1cc73d296161e74ed2ab8f071acaf00
TairineEllen <tairine.ellen@gmail.com>
2025-05-16 17:05:37 -0300

    connect backend

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -35,8 +35,17 @@ export function SignUp() {
         navigation.goBack();
     }
 
-    function handleSignUp(data: FormDataProps) {
-        console.log(data);       
+    async function handleSignUp({ name, email, password }: FormDataProps) {
+       const response = await fetch("http://127.0.0.1:3333/users", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+
+        const data = await response.json();
     }
 
     return (
