1765c8ad8e5b08caedb27b066dfb9dddca90f5b9
TairineEllen <tairine.ellen@gmail.com>
2025-05-17 16:11:08 -0300

    show name and photo of user

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,15 +1,20 @@
 import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
 import { UserPhoto } from "./UserPhoto";
-import { LogOut } from "lucide-react-native"
+import { LogOut } from "lucide-react-native";
+import { useAuth } from "@hooks/useAuth";
+
+import defaultUserPhotoImg from "@assets/userPhotoDefault.png"
 
 export function HomeHeader() {
+    const { user } = useAuth();
+
     return (
         <HStack
             bg="$gray600" 
             pt="$16" pb="$5" px="$8" alignItems="center"            
             gap="$4">
             <UserPhoto
-                source={{ uri: "https://github.com/TairineEllen.png"}}
+                source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg }
                 alt="Imagem do usuário"
                 h="$16"
                 w="$16"
@@ -19,7 +24,7 @@ export function HomeHeader() {
                 Olá,
                 </Text>
                 <Heading color="$gray100" fontSize="$md">
-                    Tairine Ellen
+                    {user.name}
                 </Heading>            
             </VStack>
             
