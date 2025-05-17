49a168e895c8556a10967564020453e0eaf9467f
TairineEllen <tairine.ellen@gmail.com>
2025-05-17 14:20:41 -0300

    create auth context

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar} from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
-import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
 import { Routes } from '@routes/index';
+import { AuthContext } from '@contexts/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular})
@@ -11,7 +12,16 @@ export default function App() {
   return (
    <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <Routes />: <Loading />}
+      <AuthContext.Provider value={{
+        user: {
+          id: "1",
+          name: "Tairine",
+          email: "tairine@email.com",
+          avatar: "tairine.png"
+        }
+      }}>
+        {fontsLoaded ? <Routes />: <Loading />}
+      </AuthContext.Provider>
    </GluestackUIProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,14 @@ import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 import { Box } from "@gluestack-ui/themed";
+import { AuthContext } from "@contexts/AuthContext";
+import { useContext } from "react";
 
 export function Routes() {
     const theme = DefaultTheme;
     theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
+
+    const contextData = useContext(AuthContext)
 
     return (
         <Box flex={1} bg="$gray700">
